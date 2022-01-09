1e1086a4267543474f68092d45b74f1deb720dd4
Rafael William <rafael.fim@hotmail.com>
2022-01-09 17:36:47 -0300

    write data to the database

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'dbconfig.dart';
 
-void main() {
-  runApp(const MaterialApp());
+final configurations = Configurations();
+
+Future<void> main() async {
+  await init();
+  runApp(const MyApp());
+
+  FirebaseFirestore.instance
+      .collection('mensagens')
+      .doc('msg2')
+      .set({'texto': 'Olá', 'from': 'Rafael', 'read': 'false'});
+}
+
+Future<void> init() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: FirebaseOptions(
+        apiKey: configurations.api,
+        appId: configurations.app,
+        messagingSenderId: configurations.messaging,
+        projectId: configurations.project),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
 }
